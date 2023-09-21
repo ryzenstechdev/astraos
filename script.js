d49b9d3b6ba42d1f44de31bac4ead8993105efc0
ryzenstech <144549189+ryzenstech2@users.noreply.github.com>
2023-09-21 15:34:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-let scrollValue = 0;
 const videoBackground = document.getElementById('video-background');
 const textContainer = document.getElementById('text-container');
 
-window.addEventListener('scroll', () => {
-    scrollValue = window.scrollY;
-    updateBackground();
+window.addEventListener('wheel', (e) => {
+    const deltaY = e.deltaY;
+    if (deltaY > 0) {
+        // Scorrimento verso il basso
+        updateBackground('down');
+    } else if (deltaY < 0) {
+        // Scorrimento verso l'alto
+        updateBackground('up');
+    }
 });
 
-function updateBackground() {
-    const translateY = Math.min(scrollValue * 0.5, 300); // Modifica il 0.5 per regolare la velocità di spostamento
+function updateBackground(direction) {
+    let translateY = 0;
+
+    if (direction === 'down') {
+        translateY = Math.min(window.scrollY * 0.5, 300); // Modifica il 0.5 per regolare la velocità di spostamento
+    } else if (direction === 'up') {
+        translateY = 0;
+    }
 
     videoBackground.style.transform = `translateY(-${translateY}px)`;
     textContainer.style.transform = `translateY(-${translateY}px)`;
 }
 
 // Chiama updateBackground() all'avvio per impostare l'aspetto iniziale
-updateBackground();
+updateBackground('up');
